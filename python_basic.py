b6dc2343997b20dc04890f620dac81b7d9a5bd8e
Amit Singh <49475880+amitrajput921998@users.noreply.github.com>
2025-07-01 23:16:58 +0530

    Add files via upload

---
+++ python_basic.py
@@ -0,0 +1,114 @@
+# # print("hello worls")
+# # name='amit'
+# # print(name)
+# # age=24
+# # if age>23:
+# #     print(age)
+
+# # x=5;
+# # y=10
+# # print(x+y)
+
+# age=23
+# hello='hello'
+# hi=True
+# print(age)
+# print(hello)
+# print(hi)
+
+# a=3
+# b=a
+# print(b)
+
+# if True:
+#     print("Correct Identation")
+#     if False:
+#         print("this is out print")
+#     print("This will print")
+# print("hello")
+
+##variables
+a=100
+#declaring and assign the variable
+# age=23
+# height=2.3
+# name="Amit"
+# is_student=True
+# print(age)
+# print(height)
+# print(name)
+# print(is_student)
+
+
+# valid variable name
+# first_name="Amit"
+# last_name="Singh"
+# print(first_name,last_name)
+# print(type(last_name))
+
+# Type conversion
+# age=25
+# print(type(age))
+
+# age_str=str(age)
+# print(age_str)
+# print(type(age_str))
+
+# number_one=2
+# number_two=3
+# print(number_one+number_two)
+# print(number_one-number_two)
+# print(number_one*number_two)
+# print(number_one/number_two)
+# print(number_one//number_two)
+# print(number_one%number_two)
+
+# age=23
+# print(age)
+
+# length=23.23
+# width=12.23
+# print(type(length))
+
+# is_marriaged=False
+
+# if is_marriaged==True:
+#     print("need to find a bride")
+# else:
+#     print("not needed")
+
+# a=12
+# b=10
+# print(a+b)
+
+# print(10/5)
+# print(21//5)
+
+#comparision operater
+# print(a==b)
+# str1="hello"
+# str2="hello"
+# print(str1==str2)
+
+# num1=45
+# num2=23
+# print(num1>num2)
+
+# x=True
+# print(not x)
+
+#  Simple calculator
+first_number=int(input("Enter the first_number :"))
+last_number=int(input("Enter the last_number :"))
+
+addition=first_number+last_number
+substraction=first_number-last_number
+multiplication=first_number*last_number
+division=first_number/last_number
+moduler=first_number%last_number
+
+print("Addition of two number is ",addition)
+print("Substraction of two number is ",substraction)
+print("Mutiplication of two number is ",multiplication)
+print("Division of two number is ",division)
+print("Moduler of two number is ",moduler)
